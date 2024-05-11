4600d68e669ecd570b809dd0fafa510a3383222e
init050 <9hoh3n1ex@protonmail.com>
2024-05-11 13:19:31 +0330

    change urls path

--- chat/urls.py
+++ chat/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .  import views
 
 urlpatterns = [
-    path('chat/', views.index, name='chat'),
-    path('<str:room_name>/', views.room, name='room'),
+    path('', views.index, name='chat'),
+    path("<str:room_name>/", views.room, name="room"),
 ]
