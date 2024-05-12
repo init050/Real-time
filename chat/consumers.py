b17a42cfa857c37e716e0c3d88917ae309a48fe6
init050 <9hoh3n1ex@protonmail.com>
2024-05-12 22:30:41 +0330

    class Chatconsumer

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,3 +1,11 @@
 from channels.generic.websocket import WebsocketConsumer
 
 
+class Chatconsumer(WebsocketConsumer):
+    
+    def connect(self):
+        pass
+    def disconnect(self, close_code):
+        pass
+    def receive(self, text_data):
+        pass
