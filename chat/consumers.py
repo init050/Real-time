f9bda66448158f1cdb16987fc956e3a85bbb9a01
init050 <9hoh3n1ex@protonmail.com>
2024-05-12 23:11:05 +0330

    write def receive and def connect

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,11 +1,14 @@
 from channels.generic.websocket import WebsocketConsumer
-
+import json
 
 class Chatconsumer(WebsocketConsumer):
     
     def connect(self):
-        pass
+        self.accept()
     def disconnect(self, close_code):
         pass
     def receive(self, text_data):
-        pass
+        text_data_json = json.load(text_data)
+        messages = text_data_json['message']
+
+        self.send(text_data=json.dump({'message':messages}))
